257ade22b13f2523ee3a63700762a63b117584e3
SonicnotsoOP <96797205+SoNiC-HeRE@users.noreply.github.com>
2023-01-17 02:32:41 +0530

    Added to the repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { marked } from "marked";
 
 function App() {
+  const [text,setText] = useState(`
+  # H1
+  ## H2
+  [title](https://www.example.com)
+  \`This is my code\`
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+
+  - First item
+  - Second item
+  - Third item
+
+  > blockquote
+
+  ![alt text](image.jpg)
+
+  **bold text**
+  `);
+
+  marked.setOptions({
+    breaks: true
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+    <h1 id="first">TypeHere</h1>
+    <textarea id="editor" onChange={(event)=>{
+      setText(event.target.value);
+    }} value={text}></textarea>
+    <h1 id="second">Preview: </h1>
+    <div id="preview" dangerouslySetInnerHTML={{__html: marked(text)}}></div>
     </div>
   );
 }
